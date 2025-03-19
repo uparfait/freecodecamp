4eb9b4de233476ac672826ddae49dfe45e8df6a5
Parfait Uwayo <167093044+uparfait@users.noreply.github.com>
2025-03-19 17:13:51 +0200

    Update index.js

--- visitmeuri/index.js
+++ visitmeuri/index.js
@@ -3,21 +3,19 @@ const app = express();
 const port = 2025;
 
 class Timestamp_service {
-  
   static parse_date(date_param) {
     let date_obj;
+
     if (!date_param) {
       date_obj = new Date();
+    } else if (/^\d+$/.test(date_param)) {
+      const timestamp = parseInt(date_param);
+      date_obj = new Date(timestamp);
     } else {
-       if (/^\d+$/.test(date_param)) {
-        date_obj = new Date(parseInt(date_param));
-      } else {
-        
-        date_obj = new Date(date_param);
-      }
+      date_obj = new Date(date_param);
     }
 
-    if (date_obj.toString() === 'Invalid Date') {
+    if (isNaN(date_obj.getTime())) {
       return { error: "Invalid Date" };
     }
 
@@ -28,14 +26,13 @@ class Timestamp_service {
   }
 }
 
-
 app.get('/', (req, res) => {
   res.send(`
     <h1>Timestamp Microservice Project</h1>
     <p>Example Of Usage Or Quick Check ✨✨✨</p>
     <ul>
       <li><a href="/api/2015-12-25">Check for this date 2015-12-25</a></li>
-      <li><a href="/api/1451001600000">Also chech this 1451001600000</a></li>
+      <li><a href="/api/1451001600000">Also check this 1451001600000</a></li>
     </ul>
     <p>You May See This Output 👍👍</p>
     <pre>{"unix":1451001600000, "utc":"Fri, 25 Dec 2015 00:00:00 GMT"}</pre>
@@ -44,12 +41,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/:date?', (req, res) => {
-  const date_param = req.params.date;
-  const result = Timestamp_service.parse_date(date_param);
+  const result = Timestamp_service.parse_date(req.params.date);
   res.json(result);
 });
 
 app.listen(port, () => {
-  console.log(`timestamp Microservice listening on port ${port}`);
+  console.log(`Timestamp Microservice listening on port ${port}`);
 });
-module.exports = app
+
+module.exports = app;
+        
