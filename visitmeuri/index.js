6330f1681bc5dc18098f697e4bd9e6cc5d502d86
Parfait Uwayo <167093044+uparfait@users.noreply.github.com>
2025-03-26 10:29:47 +0200

    Update index.js

--- visitmeuri/index.js
+++ visitmeuri/index.js
@@ -1,43 +1,55 @@
 const express = require('express');
-const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
-app.get('/api/:date?', (req, res) => {
-  let date;
-  const dateParam = req.params.date;
-
-  if (!dateParam) {
-    date = new Date();
-  } else {
-   
-    if (/^\d+$/.test(dateParam)) {
-      date = new Date(parseInt(dateParam));
-    } else {
-      date = new Date(dateParam);
-    }
+class TimestampServer {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+    this.initializeCors();
+    this.initializeRoutes();
   }
 
-  if (isNaN(date.getTime())) {
-    return res.json({ error: "Invalid Date" });
+  initializeCors() {
+    this.app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET');
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+      next();
+    });
   }
 
+  initializeRoutes() {
+    this.app.get('/api/:date?', (req, res) => {
+      let date;
+      const dateParam = req.params.date;
 
-  res.json({
-    unix: date.getTime(),
-    utc: date.toUTCString()
-  });
-});
+      if (!dateParam) {
+        date = new Date();
+      } else {
+        if (/^\d+$/.test(dateParam)) {
+          date = new Date(parseInt(dateParam));
+        } else {
+          date = new Date(dateParam);
+        }
+      }
 
+      if (isNaN(date.getTime())) {
+        return res.json({ error: "Invalid Date" });
+      }
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+      res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString()
+      });
+    });
+  }
 
-module.exports = app;
+  start() {
+    this.app.listen(this.port, () => {
+      console.log(`Server running on port ${this.port}`);
+    });
+  }
+}
+
+const server = new TimestampServer();
+server.start();
+module.exports = server.app;
