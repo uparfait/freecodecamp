340a15135e7be8dc9197ea6909b664e4366e42e8
Parfait Uwayo <167093044+uparfait@users.noreply.github.com>
2025-03-26 10:17:20 +0200

    Create index.js

---
+++ rhpmicroservice/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
