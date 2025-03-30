2194b0b7a409ced03c4ddbe822039fddf4841ac0
Parfait Uwayo <167093044+uparfait@users.noreply.github.com>
2025-03-30 11:12:20 +0200

    Update index.js

--- filemetadata/index.js
+++ filemetadata/index.js
@@ -1,49 +1,45 @@
-import express from "express"
-import cors from "cors"
-import multer from "multer"
-import path from "node:path"
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
 
-const PORT = 3002
-const app = express()
+const PORT = 3002;
+const app = express();
 
 class Server {
   constructor(app) {
-
-    this.app = app
+    this.app = app;
   }
 
-  Middeware() {
-    this.app.use(cors({ origin: '*' }))
-    this.app.use(express.json())
-    this.app.use(multer({}).single('upfile'))
+  Middleware() {
+    this.app.use(cors({ origin: '*' }));
+    this.app.use(express.json());
+    this.app.use(multer({}).single('upfile'));
   }
 
-   Router() {
+  Router() {
+    this.app.post('/api/fileanalyse', (req, res) => {
+      if (!req?.file?.type) return res.json({ error: 'No File!' });
+      res.status(200).json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size,
+      });
+    });
 
-    this.app.post('/api/fileanalyse', (req, res) =>{
-      if(!req?.file?.type) return res.json({error: 'No File!'})
-        res.status(200).json({
-            name: req.file.originalname,
-            type: req.file.mimetype,
-
-            size: req.file.size
-        })
-    })
-
-    this.app.get("*", (req, res) =>{
-        const html = path.resolve('./', "index.html")
-        res.status(200).type('text/html').sendFile(html)
-    })
-
+    this.app.get('*', (req, res) => {
+      const html = path.resolve('./', 'index.html');
+      res.status(200).type('text/html').sendFile(html);
+    });
   }
 
-   start(port) {
-    this.app.listen(port, _ => console.log("---Ready!---"))
-     export default this.app
+  start(port) {
+    this.app.listen(port, () => console.log('---Ready!---'));
+    module.exports = this.app;
   }
 }
 
-const server = new Server(app)
-server.Middeware()
-server.Router()
-server.start(PORT)
+const server = new Server(app);
+server.Middleware();
+server.Router();
+server.start(PORT);
