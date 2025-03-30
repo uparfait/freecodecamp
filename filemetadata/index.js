73b7976d09bfabdbea38ba8ad607446c0ddd1d15
Parfait Uwayo <167093044+uparfait@users.noreply.github.com>
2025-03-30 11:45:07 +0200

    Update index.js

--- filemetadata/index.js
+++ filemetadata/index.js
@@ -1,28 +1,50 @@
-const express = require('express');
-const cors = require('cors');
-const multer = require('multer');
-const path = require('path');
+import express from "express"
+import cors from "cors"
+import multer from "multer"
+import path from "node:path"
 
-const PORT = 3002;
-const app = express();
+const PORT = 3002
+const app = express()
 
-app.use(cors({ origin: '*' }));
-app.use(express.json());
-app.use(multer({}).single('upfile'));
+class Server {
+  constructor(app) {
 
-app.post('/api/fileanalyse', (req, res) => {
-    if (!req?.file?.type) return res.json({ error: 'No File!' });
-    res.status(200).json({
-      name: req.file.originalname,
-      type: req.file.mimetype,
-      size: req.file.size,
-    });
-});
+    this.app = app
+  }
 
-app.get('*', (req, res) => {
-    const html = path.resolve('./', 'index.html');
-    res.status(200).type('text/plain').end('hello users');
-});
+  Middeware() {
+    this.app.use(cors({ origin: '*' }))
+    this.app.use(express.json())
+    this.app.use(multer({}).single('upfile'))
+  }
 
-app.listen(PORT, () => console.log('---Ready!---'));
-module.exports = app;
+   Router() {
+
+    this.app.post('/api/fileanalyse', (req, res) =>{
+      if(!req?.file?.type) return res.json({error: 'No File!'})
+        res.status(200).json({
+            name: req.file.originalname,
+            type: req.file.mimetype,
+
+            size: req.file.size
+        })
+    })
+
+    this.app.get("*", (req, res) =>{
+        const html = path.resolve('./', "index.html")
+        res.status(200).type('text/html').sendFile(html)
+    })
+
+  }
+
+   start(port) {
+    this.app.listen(port, _ => console.log("---Ready!---"))
+    
+  }
+}
+
+const server = new Server(app)
+server.Middeware()
+server.Router()
+server.start(PORT)
+export default server.app
